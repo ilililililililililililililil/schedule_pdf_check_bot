128c685678af2d7a79fa071c1bdebc7e57985a0f
Ilya K <ilyazz17@yandex.ru>
2022-10-06 00:09:48 +0300

    second draft version, no bot

--- file_get_check.py
+++ file_get_check.py
@@ -1,11 +1,7 @@
-import re, sys, fitz, requests
-
-from selenium import webdriver
-from bs4 import BeautifulSoup
-
 from utils import (get_html_source,
                    get_pdf_link,
-                   get_pdf_and_save)
+                   get_pdf_and_save,
+                   pdf_content_check)
 
 from settings import (CHROME_DRIVER_PATH,
                       WORD_TO_FIND,
@@ -13,25 +9,10 @@ from settings import (CHROME_DRIVER_PATH,
                       URL_PAGE
                       )
 
+
 html_page_source = get_html_source(URL_PAGE, CHROME_DRIVER_PATH)
 pdf_link = get_pdf_link(html_page_source, SEARCH_PHRASE_FOR_PDF_LINK)
 pdf_file_name = "example_2.pdf"
 get_pdf_and_save(pdf_link=pdf_link, i_filename_to_save=pdf_file_name)
 
-doc = fitz.open(pdf_file_name)
-i = 0
-for current_page in range(len(doc)):
-    i += 1
-    if i == 1:
-        page = doc.load_page(current_page)
-        page_text = page.get_textpage('blocks')
-        # print(page_text.extractBLOCKS())
-        # data = page_text.extractWORDS()
-        # data = page_text.extractBLOCKS()
-        # if_word_exist = page_text.search(WORD_TO_FIND, quads=False)
-        data = page_text.extractBLOCKS()
-        for item in data:
-            w1, w2, w3, w4, w5, w6, w7 = item
-
-            if WORD_TO_FIND.upper() in w5.upper():
-                print(w5,'Got it!')
+result = pdf_content_check(pdf_file_name, WORD_TO_FIND)
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import re, sys, fitz, requests
 from selenium import webdriver
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
 
 def get_html_source(url_page, chrome_driver_path):
@@ -30,3 +32,24 @@ def get_pdf_link(i_html_source: str, search_phrase: str) -> str:
 def get_pdf_and_save(pdf_link: str, i_filename_to_save: str):
     response = requests.get(pdf_link)
     open(i_filename_to_save, "wb").write(response.content)
+
+
+def pdf_content_check(file_name: str, word_to_fine: str):
+    doc = fitz.open(pdf_file_name)
+    i = 0
+    for current_page in range(len(doc)):
+        i += 1
+        if i == 1:
+            page = doc.load_page(current_page)
+            page_text = page.get_textpage('blocks')
+            # print(page_text.extractBLOCKS())
+            # data = page_text.extractWORDS()
+            # data = page_text.extractBLOCKS()
+            # if_word_exist = page_text.search(WORD_TO_FIND, quads=False)
+            data = page_text.extractBLOCKS()
+            for item in data:
+                w1, w2, w3, w4, w5, w6, w7 = item
+
+                if word_to_fine.upper() in w5.upper():
+                    o_result = str(w5) + 'Got it!'
+    return o_result
