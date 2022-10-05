4903aab358ead3f511de9bacdfb0e996ec9a11b1
Ilya K <ilyazz17@yandex.ru>
2022-10-06 01:13:37 +0300

    third version. Parsing html works. it could find the pdf link correctly now

--- file_get_check.py
+++ file_get_check.py
@@ -10,9 +10,10 @@ from settings import (CHROME_DRIVER_PATH,
                       )
 
 
-html_page_source = get_html_source(URL_PAGE, CHROME_DRIVER_PATH)
+# html_page_source = get_html_source(URL_PAGE, CHROME_DRIVER_PATH)
+html_page_source = ''
 pdf_link = get_pdf_link(html_page_source, SEARCH_PHRASE_FOR_PDF_LINK)
-pdf_file_name = "example_2.pdf"
-get_pdf_and_save(pdf_link=pdf_link, i_filename_to_save=pdf_file_name)
 
-result = pdf_content_check(pdf_file_name, WORD_TO_FIND)
+pdf_file_name = "example_2.pdf"
+# get_pdf_and_save(pdf_link=pdf_link, i_filename_to_save=pdf_file_name)
+# result = pdf_content_check(pdf_file_name, WORD_TO_FIND)
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
-import re, sys, fitz, requests
-from selenium import webdriver
+import re, sys
+import fitz, requests
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
@@ -15,17 +15,33 @@ def get_html_source(url_page, chrome_driver_path):
     return o_html_source
 
 
-def get_pdf_link(i_html_source: str, search_phrase: str) -> str:
+def get_pdf_link(i_html_source: str, search_phrase: str):
 
-    soup = BeautifulSoup(i_html_source, "html.parser")
-    txt_data_array = soup.select_one("a")
-    data = txt_data_array.find_all("title")
+    with open("example.html") as fp:
+        soup = BeautifulSoup(fp, "html.parser")
 
-    for line in data:
-        print(str(line))
+    # soup = BeautifulSoup(i_html_source, "html.parser")
+    # txt_data_array = soup.select_one("div.text_content_block")
+    data = soup.find_all("a")
+    pdf_link: str = ''
+    for a_item in data:
+        event_title: str = a_item.text
+        link_title: str = a_item.get("title")
+        if link_title:
+            if link_title.upper() == search_phrase.upper():
+                pdf_link = a_item.get("href")
 
-    o_link: str = 'empty'
+            if pdf_link is False:
+                text = "ДЕКАБРЬ"
+                if event_title:
+                    if text.upper() in event_title.upper():
+                        pdf_link = a_item.get("href")
 
+    if pdf_link:
+        first_link_part = "https://2011.bolshoi.ru" 
+        o_link = first_link_part + pdf_link
+    else:
+        o_link = 'NO_LINK'
     return o_link
 
 
