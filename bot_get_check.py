93b726c356e10058ae54d83ddf74d8bbc4f1da0b
Ilya K <ilyazz17@yandex.ru>
2022-10-06 22:50:22 +0300

    commit 2nd version bot

--- bot_get_check.py
+++ bot_get_check.py
@@ -2,6 +2,7 @@ import time
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+import outcome
 from config_bot import TOKEN
 from utils import (get_html_source,
                    get_pdf_link,
@@ -19,13 +20,20 @@ from settings import (CHROME_DRIVER_PATH,
                       )
 
 def main():
+    output: str = ''
     stop_working = 0
-    delay = 600
-    print('Program starts at:', get_datetime_now('full'))
-    print('Delay between requests is: ', delay, 'seconds')
-    print('_________________________________________________')
+    delay = 60
+    iterations = 0
+    # print('Program starts at:', get_datetime_now('full'))
+    # print('Delay between requests is: ', delay, 'seconds')
+    # print('_________________________________________________')
+
+    output += '\n' + 'Program starts at: ' + str(get_datetime_now('full'))
+    output += '\n' + '___________________________'
 
     while stop_working != 1:
+        stop_working = 1
+        iterations += 1
         html_page_source = get_html_source(URL_PAGE, CHROME_DRIVER_PATH)
         # html_page_source = '' # manual file for
         pdf_link = get_pdf_link(html_page_source, SEARCH_PHRASE_FOR_PDF_LINK)
@@ -36,14 +44,21 @@ def main():
         result = pdf_content_check(pdf_file_name, WORD_TO_FIND, PIC_PATH)
 
         for key, value in result.items():
-            print('one more try at:', get_datetime_now('small'))
-            print('Key: ', key, ', result: ', value)
-            print('_________________________________________________')
+            # print('one more try at:', get_datetime_now('small'))
+            # print('Key: ', key, ', result: ', value)
+            # print('_________________________________________________')
+            #output += '\n' + 'one more try at:' + str(get_datetime_now('small'))
+            output += '\n' + str('Key: ') + str(key) + str(', result: ') + str(value)
+            #output += '\n' + '___________________________'
+            #output += '\n' 
 
             if key == WORD_TO_FIND.upper():
                 stop_working = 1
 
-        time.sleep(delay)
+        #time.sleep(delay)
+        #if iterations > 1:
+        #    stop_working = 1
+    return output
 
 
 bot = Bot(token=TOKEN)
@@ -55,30 +70,40 @@ async def start(message: types.Message):
     """Добрый день! \n
     Этот бот предназначен для регулярной автоматической проверки 
     графика продажи билетов на спектакль в Большой театр. \n
-    Это означает, что после настройки и команды от пользователя
-    Бот будет проверять раздел ПРАВИЛА ПРОДАЖИ БИЛЕТОВ 
+    Это означает, что Бот будет проверять раздел ПРАВИЛА ПРОДАЖИ БИЛЕТОВ 
     а именно
     График предварительной продажи билетов на ДЕКАБРЬ 2022 г. 
 
-    По умолчанию бот ждет появления в графике продаж данных о спектакле Щелкунчик
-    проверка происходит раз в 30 минут
-    При желании эти настройки можно поменять. 
-    Как их поменять? - Введите команду help""")
+    По умолчанию бот ждет появления в графике продаж спектакля "Щелкунчик"
+    проверка происходит после получения любого сообщения
+    Напишите любое сообщение и бот проверит есть ли щелкунчик в расписании
 
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.reply("""Доступные настройки:
-    - Команда : описание
-    - delay : задержка между проверками данных на сайте, 
-    указывается в минутах
-    (минимальный интервал 30 минут, максимальный 1 раз в день)
+    """)
+# При желании эти настройки можно поменять. 
+#    Как их поменять? - Введите команду help
 
-    - show : название спектакля, рекомендуется указывать 1, наиболее характерное слово
-    (даже если название спектакля состоит из нескольких слов)
+#@dp.message_handler(commands=['help'])
+#async def process_help_command(message: types.Message):
+#    await message.reply("""Доступные настройки:
+#    - Команда : описание
+#    - delay : задержка между проверками данных на сайте, 
+#    указывается в минутах
+#    (минимальный интервал 30 минут, максимальный 1 раз в день)
+#
+#    - show : название спектакля, рекомендуется указывать 1, наиболее характерное слово
+#    (даже если название спектакля состоит из нескольких слов)
+#
+#    Примеры использования команд(без кавычек и доп. символов):
+#    delay 30
+#    delay 180
+#    show каренина
+#    show озеро """)
 
-    Примеры использования команд(без кавычек и доп. символов):
-    delay 30
-    delay 180
-    show каренина
-    show озеро """)
+@dp.message_handler() #commands = ("stock", "s"),commands_prefix = "/!"
+async def record(message: types.message):
+    result = main()
+    await message.bot.send_message(message.from_user.id, result)
 
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
